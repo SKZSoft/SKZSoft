6a9b1d43a367f5f8f0093975f973c57108a59882
SKZCartoons <SKZCartoons@589015e6-7a3c-4bcb-86c8-e620344334e9>
2018-03-26 03:18:11 +0000

    Removed sign in/sign out/switch account buttons and code.

--- trunk/SKZTweets/SKZTweets/Forms/frmMainWindow.Designer.cs
+++ trunk/SKZTweets/SKZTweets/Forms/frmMainWindow.Designer.cs
@@ -22,11 +22,6 @@
             this.menuStrip2 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuFileExit = new System.Windows.Forms.ToolStripMenuItem();
-            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuConnectSignIn = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.switchAccountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.signOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tweetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.retweeterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.threadCreatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -45,14 +40,11 @@
             this.viewChangelogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewKnownIssuesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.tsbSignIn = new System.Windows.Forms.ToolStripButton();
-            this.tsbSwitchAccount = new System.Windows.Forms.ToolStripButton();
-            this.tsbSignout = new System.Windows.Forms.ToolStripButton();
+            this.tscTwitterAccount = new System.Windows.Forms.ToolStripComboBox();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.tsbRetweeter = new System.Windows.Forms.ToolStripButton();
             this.tsbThreadCreator = new System.Windows.Forms.ToolStripButton();
             this.tsbDM = new System.Windows.Forms.ToolStripButton();
-            this.tscTwitterAccount = new System.Windows.Forms.ToolStripComboBox();
             this.menuStrip2.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -66,7 +58,6 @@
             // 
             this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
-            this.connectToolStripMenuItem,
             this.tweetsToolStripMenuItem,
             this.schedulesToolStripMenuItem,
             this.toolsToolStripMenuItem});
@@ -91,45 +82,6 @@
             this.mnuFileExit.Text = "E&xit";
             this.mnuFileExit.Click += new System.EventHandler(this.mnuFileExit_Click);
             // 
-            // connectToolStripMenuItem
-            // 
-            this.connectToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.mnuConnectSignIn,
-            this.toolStripSeparator1,
-            this.switchAccountToolStripMenuItem,
-            this.signOutToolStripMenuItem});
-            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
-            this.connectToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
-            this.connectToolStripMenuItem.Text = "&Connect";
-            // 
-            // mnuConnectSignIn
-            // 
-            this.mnuConnectSignIn.Image = global::SKZSoft.SKZTweets.Properties.Resources.MyWork_24x;
-            this.mnuConnectSignIn.Name = "mnuConnectSignIn";
-            this.mnuConnectSignIn.Size = new System.Drawing.Size(155, 22);
-            this.mnuConnectSignIn.Text = "&Sign in";
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(152, 6);
-            // 
-            // switchAccountToolStripMenuItem
-            // 
-            this.switchAccountToolStripMenuItem.Image = global::SKZSoft.SKZTweets.Properties.Resources.Disconnect_16x;
-            this.switchAccountToolStripMenuItem.Name = "switchAccountToolStripMenuItem";
-            this.switchAccountToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
-            this.switchAccountToolStripMenuItem.Text = "S&witch account";
-            this.switchAccountToolStripMenuItem.Click += new System.EventHandler(this.switchAccountToolStripMenuItem_Click);
-            // 
-            // signOutToolStripMenuItem
-            // 
-            this.signOutToolStripMenuItem.Image = global::SKZSoft.SKZTweets.Properties.Resources.UserError_16x;
-            this.signOutToolStripMenuItem.Name = "signOutToolStripMenuItem";
-            this.signOutToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
-            this.signOutToolStripMenuItem.Text = "Sign &Out";
-            this.signOutToolStripMenuItem.Click += new System.EventHandler(this.signOutToolStripMenuItem_Click);
-            // 
             // tweetsToolStripMenuItem
             // 
             this.tweetsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -143,7 +95,7 @@
             // 
             this.retweeterToolStripMenuItem.Image = global::SKZSoft.SKZTweets.Properties.Resources.retweet;
             this.retweeterToolStripMenuItem.Name = "retweeterToolStripMenuItem";
-            this.retweeterToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.retweeterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.retweeterToolStripMenuItem.Text = "&Retweeter";
             this.retweeterToolStripMenuItem.Click += new System.EventHandler(this.retweeterToolStripMenuItem_Click);
             // 
@@ -151,7 +103,7 @@
             // 
             this.threadCreatorToolStripMenuItem.Image = global::SKZSoft.SKZTweets.Properties.Resources.DocumentOutline_16x;
             this.threadCreatorToolStripMenuItem.Name = "threadCreatorToolStripMenuItem";
-            this.threadCreatorToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.threadCreatorToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.threadCreatorToolStripMenuItem.Text = "&Thread Creator";
             this.threadCreatorToolStripMenuItem.Click += new System.EventHandler(this.threadCreatorToolStripMenuItem_Click);
             // 
@@ -265,9 +217,6 @@
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tscTwitterAccount,
-            this.tsbSignIn,
-            this.tsbSwitchAccount,
-            this.tsbSignout,
             this.toolStripSeparator2,
             this.tsbRetweeter,
             this.tsbThreadCreator,
@@ -278,35 +227,12 @@
             this.toolStrip1.TabIndex = 5;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // tsbSignIn
+            // tscTwitterAccount
             // 
-            this.tsbSignIn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.tsbSignIn.Image = global::SKZSoft.SKZTweets.Properties.Resources.MyWork_24x;
-            this.tsbSignIn.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.tsbSignIn.Name = "tsbSignIn";
-            this.tsbSignIn.Size = new System.Drawing.Size(23, 22);
-            this.tsbSignIn.Text = "Sign in";
-            // 
-            // tsbSwitchAccount
-            // 
-            this.tsbSwitchAccount.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.tsbSwitchAccount.Image = global::SKZSoft.SKZTweets.Properties.Resources.Disconnect_16x;
-            this.tsbSwitchAccount.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.tsbSwitchAccount.Name = "tsbSwitchAccount";
-            this.tsbSwitchAccount.Size = new System.Drawing.Size(23, 22);
-            this.tsbSwitchAccount.Text = "toolStripButton2";
-            this.tsbSwitchAccount.ToolTipText = "Switch Account";
-            this.tsbSwitchAccount.Click += new System.EventHandler(this.tsbSwitchAccount_Click);
-            // 
-            // tsbSignout
-            // 
-            this.tsbSignout.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.tsbSignout.Image = global::SKZSoft.SKZTweets.Properties.Resources.UserError_16x;
-            this.tsbSignout.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.tsbSignout.Name = "tsbSignout";
-            this.tsbSignout.Size = new System.Drawing.Size(23, 22);
-            this.tsbSignout.ToolTipText = "Sign out";
-            this.tsbSignout.Click += new System.EventHandler(this.tsbSignout_Click);
+            this.tscTwitterAccount.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tscTwitterAccount.Name = "tscTwitterAccount";
+            this.tscTwitterAccount.Size = new System.Drawing.Size(121, 25);
+            this.tscTwitterAccount.Sorted = true;
             // 
             // toolStripSeparator2
             // 
@@ -343,13 +269,6 @@
             this.tsbDM.Text = "toolStripButton1";
             this.tsbDM.Click += new System.EventHandler(this.tsbDM_Click);
             // 
-            // tscTwitterAccount
-            // 
-            this.tscTwitterAccount.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.tscTwitterAccount.Name = "tscTwitterAccount";
-            this.tscTwitterAccount.Size = new System.Drawing.Size(121, 25);
-            this.tscTwitterAccount.Sorted = true;
-            // 
             // frmMainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -379,16 +298,10 @@
         private System.Windows.Forms.MenuStrip menuStrip2;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem mnuFileExit;
-        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem mnuConnectSignIn;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem tweetsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem retweeterToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem threadCreatorToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem switchAccountToolStripMenuItem;
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton tsbSignIn;
-        private System.Windows.Forms.ToolStripButton tsbSwitchAccount;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripButton tsbRetweeter;
         private System.Windows.Forms.ToolStripButton tsbThreadCreator;
@@ -403,8 +316,6 @@
         private System.Windows.Forms.ToolStripMenuItem openUnhandledLogFileToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
         private System.Windows.Forms.ToolStripMenuItem openLogDirectoryToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem signOutToolStripMenuItem;
-        private System.Windows.Forms.ToolStripButton tsbSignout;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
         private System.Windows.Forms.ToolStripMenuItem viewChangelogToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem viewKnownIssuesToolStripMenuItem;
