397b791387381792e98f3395bf9704d88bf1da4e
SKZCartoons <SKZCartoons@589015e6-7a3c-4bcb-86c8-e620344334e9>
2018-05-09 21:00:42 +0000

    1st cut getting SVGs to load.
    Images way too small. However they are correct size when SVG library is used as CODE rather than as a package.

---
+++ trunk/SKZStrips/SKZStrips/Controller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace SKZStrips
+{
+    public class Controller
+    {
+        // TODO softcode this and put it somewhere sensible
+        const string configPath = @"d:\GFX\";
+        const string configFilename = @"SKZStrips config.xml";
+
+        private List<ImagePalette> m_imagePalettes;
+
+        public List<ImagePalette> ImagePalettes {  get { return m_imagePalettes; } }
+
+        public frmMain Initialise()
+        {
+            XmlDocument xmlConfig = new XmlDocument();
+            string fullConfigPath = Path.Combine(configPath, configFilename);
+            xmlConfig.Load(fullConfigPath);
+
+            XmlNode palettes = xmlConfig.SelectSingleNode("/config/palettes");
+            m_imagePalettes = new List<ImagePalette>();
+            
+            foreach(XmlNode paletteConfig in palettes.ChildNodes)
+            {
+                ImagePalette paletteForm = new ImagePalette();
+                paletteForm.Initialise(paletteConfig, configPath);
+                m_imagePalettes.Add(paletteForm);
+            }
+
+
+            frmMain mainForm = new frmMain();
+            mainForm.Initialise(this);
+            return mainForm;
+        }
+        
+    }
+}
---
+++ trunk/SKZStrips/SKZStrips/ImagePalette.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace SKZStrips
+{
+    public partial class ImagePalette : Form
+    {
+        public ImagePalette()
+        {
+            InitializeComponent();
+        }
+
+        public void Initialise(XmlNode config, string basePath)
+        {
+            imagePicker.Initialise(config, basePath);
+        }
+    }
+}
