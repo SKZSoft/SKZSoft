06de282926d43960f251fc1d9ad71a47da4a63a3
SKZCartoons <SKZCartoons@589015e6-7a3c-4bcb-86c8-e620344334e9>
2018-07-11 08:54:01 +0000

    Font selection working

--- trunk/SKZBanners/SKZBanners/frmMain.Designer.cs
+++ trunk/SKZBanners/SKZBanners/frmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SKZBanners
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.cmbFonts = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(120, 80);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(438, 66);
+            this.txtText.TabIndex = 0;
+            this.txtText.Text = "Buy \"The Little Book of Brexit Logic: recipes for disaster\"\r\nfor only £10 plus p&" +
+    "p from www.skzcartoons.co.uk!\r\n";
+            // 
+            // cmbFonts
+            // 
+            this.cmbFonts.FormattingEnabled = true;
+            this.cmbFonts.Location = new System.Drawing.Point(120, 168);
+            this.cmbFonts.Name = "cmbFonts";
+            this.cmbFonts.Size = new System.Drawing.Size(226, 21);
+            this.cmbFonts.TabIndex = 1;
+            this.cmbFonts.SelectedIndexChanged += new System.EventHandler(this.cmbFonts_SelectedIndexChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(910, 466);
+            this.Controls.Add(this.cmbFonts);
+            this.Controls.Add(this.txtText);
+            this.Name = "frmMain";
+            this.Text = "SKZ Banners";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.ComboBox cmbFonts;
     }
 }
 
--- trunk/SKZBanners/SKZBanners/frmMain.cs
+++ trunk/SKZBanners/SKZBanners/frmMain.cs
@@ -10,11 +10,52 @@ using System.Windows.Forms;
 
 namespace SKZBanners
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        private Dictionary<string, FontFamily> m_fonts;
+
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            LoadFonts();
+        }
+
+        private void LoadFonts()
+        {
+            m_fonts = new Dictionary<string, FontFamily>();
+
+            foreach (FontFamily font in System.Drawing.FontFamily.Families)
+            {
+                m_fonts.Add(font.Name, font);
+            }
+
+            int defaultIndex = -1;
+            foreach(KeyValuePair<string, FontFamily> kvp in m_fonts)
+            {
+                cmbFonts.Items.Add(kvp.Value.Name);
+                if(kvp.Value.Name == "Comic Sans MS")
+                {
+                    defaultIndex = cmbFonts.Items.Count - 1;
+                }
+            }
+
+            cmbFonts.SelectedIndex = defaultIndex;
+
+
+        }
+
+        private void cmbFonts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cmbFonts.SelectedIndex > -1)
+            {
+                string fontName = cmbFonts.Items[cmbFonts.SelectedIndex].ToString();
+                FontFamily font = m_fonts[fontName];
+                txtText.Font = new Font(font, 10);
+            }
         }
     }
 }
