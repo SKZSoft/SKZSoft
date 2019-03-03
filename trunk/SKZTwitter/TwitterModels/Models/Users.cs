06df03f84d97d88749f6407aee23c25b7adbd01a
SKZCartoons <SKZCartoons@589015e6-7a3c-4bcb-86c8-e620344334e9>
2019-03-03 13:45:36 +0000

    Missed from Commit

---
+++ trunk/SKZTwitter/TwitterModels/Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKZSoft.Twitter.TwitterModels
+{
+    /// <summary>
+    /// Not deserializable
+    /// </summary>
+    public class Users
+    {
+        public List<User> users { get; set; }
+        public string next_cursor_str { get; set; }
+
+        public Users()
+        {
+            users = new List<User>();
+        }
+    }
+}
