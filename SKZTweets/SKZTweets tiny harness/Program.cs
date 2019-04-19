28452fc30c48ca84e32c4af33f5d270b6275dc08
SKZCartoons <26833903+SKZSoft@users.noreply.github.com>
2019-04-19 21:09:10 +0000

    Comments for clarity

--- SKZTweets/SKZTweets tiny harness/Program.cs
+++ SKZTweets/SKZTweets tiny harness/Program.cs
@@ -21,10 +21,13 @@ namespace SKZTweets_tiny_harness
             frmSplash splash = new frmSplash();
             splash.Show();
 
+            // Now start the controller.
             Controller controller = new Controller(splash);
             frmMain main = new frmMain(controller);
             controller.Initialise(main);
 
+            // Run the application, giving it the splash screen to receive messages initially.
+            // The main form will take over when the Controller is initialised.
             Application.Run(splash);
         }
     }
