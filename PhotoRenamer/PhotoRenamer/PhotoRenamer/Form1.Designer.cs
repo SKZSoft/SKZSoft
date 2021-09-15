f0a8994200fece2e0089e815782c9b4474223643
SKZCartoons <26833903+SKZSoft@users.noreply.github.com>
2021-09-15 20:31:39 +0000

    Needs preview list of folders cos it's renaming twitter stuff

---
+++ PhotoRenamer/PhotoRenamer/PhotoRenamer/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace PhotoRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstPaths = new System.Windows.Forms.ListBox();
+            this.btnPathsClear = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Paths";
+            // 
+            // lstPaths
+            // 
+            this.lstPaths.AllowDrop = true;
+            this.lstPaths.FormattingEnabled = true;
+            this.lstPaths.Location = new System.Drawing.Point(86, 29);
+            this.lstPaths.Name = "lstPaths";
+            this.lstPaths.Size = new System.Drawing.Size(702, 355);
+            this.lstPaths.TabIndex = 2;
+            this.lstPaths.DragDrop += new System.Windows.Forms.DragEventHandler(this.lstPaths_DragDrop);
+            this.lstPaths.DragEnter += new System.Windows.Forms.DragEventHandler(this.lstPaths_DragEnter);
+            // 
+            // btnPathsClear
+            // 
+            this.btnPathsClear.Location = new System.Drawing.Point(5, 73);
+            this.btnPathsClear.Name = "btnPathsClear";
+            this.btnPathsClear.Size = new System.Drawing.Size(75, 23);
+            this.btnPathsClear.TabIndex = 3;
+            this.btnPathsClear.Text = "&Clear";
+            this.btnPathsClear.UseVisualStyleBackColor = true;
+            this.btnPathsClear.Click += new System.EventHandler(this.btnPathsClear_Click);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(86, 390);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 4;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnPathsClear);
+            this.Controls.Add(this.lstPaths);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstPaths;
+        private System.Windows.Forms.Button btnPathsClear;
+        private System.Windows.Forms.Button btnProcess;
+    }
+}
+
--- PhotoRenamer/PhotoRenamer/PhotoRenamer/Properties/Resources.Designer.cs
+++ PhotoRenamer/PhotoRenamer/PhotoRenamer/Properties/Resources.Designer.cs
@@ -8,10 +8,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SKZSoft.Common.Logging.GUI {
-    using System;
-    
-    
+
+namespace PhotoRenamer.Properties
+{
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,71 +18,52 @@ namespace SKZSoft.Common.Logging.GUI {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Strings {
-        
+    internal class Resources
+    {
+
         private static global::System.Resources.ResourceManager resourceMan;
-        
+
         private static global::System.Globalization.CultureInfo resourceCulture;
-        
+
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Strings() {
+        internal Resources()
+        {
         }
-        
+
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
-            get {
-                if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SKZSoft.Common.Logging.GUI.Strings", typeof(Strings).Assembly);
+        internal static global::System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if ((resourceMan == null))
+                {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PhotoRenamer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-        
+
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
-            get {
+        internal static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
                 return resourceCulture;
             }
-            set {
+            set
+            {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to The following errors were found in the options:.
-        /// </summary>
-        internal static string ErrorsFound {
-            get {
-                return ResourceManager.GetString("ErrorsFound", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to No Level selected.
-        /// </summary>
-        internal static string NoLevel {
-            get {
-                return ResourceManager.GetString("NoLevel", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Number of days before log deletion must be a positive integer..
-        /// </summary>
-        internal static string NoOfDaysValidate {
-            get {
-                return ResourceManager.GetString("NoOfDaysValidate", resourceCulture);
             }
         }
     }
