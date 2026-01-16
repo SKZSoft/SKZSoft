7700a32d5ea7e91423e66f0087d21fe51870e930
SKZSoft <skzcartoons@gmail.com>
2026-01-16 08:55:12 +0100

    File search initial version TODO: catch RETURN key

---
+++ FileSearch/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace FileSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtPath = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtIncludeOnly = new TextBox();
+            txtExclude = new TextBox();
+            label3 = new Label();
+            btnSearch = new Button();
+            grdResults = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)grdResults).BeginInit();
+            SuspendLayout();
+            // 
+            // txtPath
+            // 
+            txtPath.Location = new Point(114, 3);
+            txtPath.Name = "txtPath";
+            txtPath.Size = new Size(584, 23);
+            txtPath.TabIndex = 0;
+            txtPath.Text = "a:\\";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Path";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Include Only";
+            // 
+            // txtIncludeOnly
+            // 
+            txtIncludeOnly.Location = new Point(114, 43);
+            txtIncludeOnly.Multiline = true;
+            txtIncludeOnly.Name = "txtIncludeOnly";
+            txtIncludeOnly.Size = new Size(229, 150);
+            txtIncludeOnly.TabIndex = 3;
+            txtIncludeOnly.Text = ".avi";
+            // 
+            // txtExclude
+            // 
+            txtExclude.Location = new Point(469, 43);
+            txtExclude.Multiline = true;
+            txtExclude.Name = "txtExclude";
+            txtExclude.Size = new Size(229, 150);
+            txtExclude.TabIndex = 5;
+            txtExclude.Text = "\\r\\";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(416, 43);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Exclude";
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(114, 199);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(75, 23);
+            btnSearch.TabIndex = 6;
+            btnSearch.Text = "&Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // grdResults
+            // 
+            grdResults.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            grdResults.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            grdResults.Location = new Point(12, 228);
+            grdResults.Name = "grdResults";
+            grdResults.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            grdResults.Size = new Size(740, 337);
+            grdResults.TabIndex = 7;
+            grdResults.CellDoubleClick += grdResults_CellDoubleClick;
+            grdResults.DoubleClick += grdResults_DoubleClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(764, 577);
+            Controls.Add(grdResults);
+            Controls.Add(btnSearch);
+            Controls.Add(txtExclude);
+            Controls.Add(label3);
+            Controls.Add(txtIncludeOnly);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPath);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)grdResults).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtPath;
+        private Label label1;
+        private Label label2;
+        private TextBox txtIncludeOnly;
+        private TextBox txtExclude;
+        private Label label3;
+        private Button btnSearch;
+        private DataGridView grdResults;
+    }
+}
---
+++ FileSearch/Form1.cs
@@ -0,0 +1,107 @@
+namespace FileSearch
+{
+    using System.Diagnostics;
+    using System.IO;
+    public partial class Form1 : Form
+    {
+        Dictionary<string, FileInfo> m_files = new Dictionary<string, FileInfo>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            grdResults.Rows.Clear();
+            grdResults.Columns.Clear();
+            grdResults.Columns.Add("File", "File");
+            grdResults.Refresh();
+
+            DirectoryInfo di = new DirectoryInfo(txtPath.Text);
+            m_files = new Dictionary<string, FileInfo>();
+            Scan(di);
+
+            string[] includeOnly = txtIncludeOnly.Text.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            string[] exclude = txtExclude.Text.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            // check if it's in the "include only" list
+            foreach (KeyValuePair<string, FileInfo> kvp in m_files)
+            {
+                bool include = true;    // assume it's going in
+                if (includeOnly.Length > 0 || exclude.Length > 0)
+                {
+                    include = false;    // now pessimism
+
+                    if (includeOnly.Length > 0)
+                    {
+                        include = false;
+                        FileInfo fi = kvp.Value;
+                        if (includeOnly.Length > 0)
+                        {
+                            foreach (string toInclude in includeOnly)
+                            {
+                                if (fi.FullName.Contains(toInclude))
+                                {
+                                    include = true;
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            include = true;
+                        }
+
+                        // if it's included, is it subsequently excluded?
+                        if (include)
+                        {
+                            foreach (string toExclude in exclude)
+                            {
+                                if (fi.FullName.Contains(toExclude))
+                                {
+                                    include = false;
+                                }
+                            }
+                        }
+
+                        // add to results
+                        if (include)
+                        {
+                            grdResults.Rows.Add(fi.FullName);
+                        }
+
+                    }
+                }
+            }
+            grdResults.AutoResizeColumns();
+
+        }
+
+        private void Scan(DirectoryInfo di)
+        {
+            foreach (FileInfo fi in di.GetFiles())
+            {
+                m_files.Add(fi.FullName, fi);
+            }
+
+            foreach (DirectoryInfo di2 in di.GetDirectories())
+            {
+                Scan(di2);
+            }
+        }
+
+        private void grdResults_DoubleClick(object sender, EventArgs e)
+        {
+        }
+
+        private void grdResults_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = e.RowIndex;
+            //DataGridViewAutoSizeRowMode row = 
+            string file = grdResults.Rows[row].Cells[0].Value.ToString();
+            ProcessStartInfo pi = new ProcessStartInfo(file) { UseShellExecute = true };
+            Process.Start(pi);
+        }
+    }
+}
